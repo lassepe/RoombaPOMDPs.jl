95070bed8434c5679ca274e37dae3e68d5b78275
lassepe <lasse.peters@tuhh.de>
2019-06-19 16:34:49 +0200

    Publication ready plots

--- scripts/analyze_results.jl
+++ scripts/analyze_results.jl
@@ -3,18 +3,33 @@ using DataFramesMeta
 using Gadfly
 using CSV
 using Statistics
+using LaTeXStrings
 
 function plot_results(data::DataFrame;
                       reward_type::Symbol=:discounted_reward,
                       show_value_sem::Bool=true, show_value_violin::Bool=true,
                       show_nsteps::Bool=true, show_fst::Bool=true)
-    plot_stack = []
 
-	Gadfly.set_default_plot_size(40cm,30cm)
-    detailed_theme = Gadfly.Theme(major_label_font="cmr10",
-                                  minor_label_font="cmr10",
-                                  major_label_font_size=12pt,
-                                  minor_label_font_size=0pt, key_position=:none)
+    legend_guide = Guide.colorkey(title="Legend")
+    default_font = "cmr10"
+
+    plot_stack = []
+    default_theme = Gadfly.Theme(key_title_font=default_font,
+                                 key_label_font=default_font,
+                                 major_label_font=default_font,
+                                 minor_label_font=default_font,
+                                 major_label_font_size=10pt,
+                                 minor_label_font_size=8pt, key_position=:none)
+
+    # and some more for the remaining plots
+    for i in 1:10
+        Gadfly.push_theme(default_theme)
+    end
+    # the theme for the first plot
+    first_plot_theme = copy(default_theme)
+    first_plot_theme.key_position=:top
+    Gadfly.push_theme(first_plot_theme)
+
 
     # Collect some statistics. One row per policy key
 	df_stats = DataFrame(policy_key=String[], MeanValue=Float64[], SEMValue=Float64[])
@@ -27,28 +42,33 @@ function plot_results(data::DataFrame;
         # Error bar plot includid SEM
         value_errorbar_plot = plot(x=df_stats.policy_key, y=df_stats.MeanValue,
                                    ymin=(df_stats.MeanValue - df_stats.SEMValue), ymax=(df_stats.MeanValue + df_stats.SEMValue),
-                                   color=df_stats.policy_key, Geom.point, Geom.errorbar, Guide.xlabel("Policy"), Guide.ylabel("Cumulative Discounted Reward"),
-                                   detailed_theme)
+                                   color=df_stats.policy_key, Geom.point, Geom.errorbar, Guide.xlabel("Policy"), Guide.ylabel("Cumulative Discounted Reward (SEM)", orientation=:vertical),
+                                   legend_guide)
         push!(plot_stack, value_errorbar_plot)
+        Gadfly.pop_theme()
     end
 
     if show_value_violin
         # violin plot for reward distribution
-        value_violin_plot = plot(data, x=:policy_key, y=reward_type, color=:policy_key, Geom.violin, detailed_theme)
+        value_violin_plot = plot(data, x=:policy_key, y=reward_type, color=:policy_key, Geom.violin,
+                                 Guide.xlabel("Policy"), Guide.ylabel("Cumulative Discounted Reward (PDF)", orientation=:vertical))
         push!(plot_stack, value_violin_plot)
+        Gadfly.pop_theme()
     end
 
     if show_nsteps
-        nsteps_violin_plot = plot(data, x=:policy_key, y=:n_steps, color=:policy_key, Geom.violin, detailed_theme)
+        nsteps_violin_plot = plot(data, x=:policy_key, y=:n_steps, color=:policy_key, Geom.violin)
         push!(plot_stack, nsteps_violin_plot)
+        Gadfly.pop_theme()
     end
 
     if show_fst
         # historgram over success
         final_state_type_plot = plot(data, xgroup=:policy_key, x=:final_state_type,
                                      color=:policy_key,
-                                     Geom.subplot_grid(Geom.histogram), detailed_theme)
+                                     Geom.subplot_grid(Geom.histogram))
         push!(plot_stack, final_state_type_plot)
+        Gadfly.pop_theme()
     end
 
     vstack(plot_stack...)
--- scripts/generate_plots.jl
+++ scripts/generate_plots.jl
@@ -6,9 +6,19 @@ rename(s::String, old::String="MostLikelyStateController", new::String="MLRA") =
 
 function create_eval_plot(;filename::String="$(@__DIR__)/../results/final_results/data_eval_final.csv",
                           outname::String="$(@__DIR__)/../results/final_results/plots/lp_eval_plot.pdf",
-                          dims=(30cm, 20cm))
+                          dims=(25cm, 15cm))
     data_po = CSV.read(filename)
     p = plot_results(data_po, show_nsteps=false, show_fst=false)
     draw(PDF(outname, dims...), p)
     #run(`xdg-open $outname`)
 end
+
+# Renaming:
+#
+# d = CSV.read("results/final_results/data_po.csv")
+# d = @transform(d, policy_key=rename.(:policy_key, "MostLikelyStateController", "MLRA"))
+# d = @transform(d, policy_key=rename.(:policy_key, "DESPOT_defaultPolicy", "DESPOT-rollout"))
+# d = @transform(d, policy_key=rename.(:policy_key, "DESPOT_defaultPolicy", "DESPOT-rollout"))
+# d = @transform(d, policy_key=rename.(:policy_key, "POMCPOW_rolloutEstimate", "POMCPOW-rollout"))
+# d = @transform(d, policy_key=rename.(:policy_key, "POMCPOW_analyticValueEstimate", "POMCPOW-analytic"))
+# d = CSV.write("results/final_results/data_eval_final.csv")
