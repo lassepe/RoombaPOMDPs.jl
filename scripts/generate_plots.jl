ceb6c800fb9c8dd9ab89c4c5abd308079af2e1c7
lassepe <lasse.peters@tuhh.de>
2019-06-21 15:35:03 +0200

    Plot for all reward types

--- scripts/generate_plots.jl
+++ scripts/generate_plots.jl
@@ -4,33 +4,35 @@ import Cairo, Fontconfig
 
 rename(s::String, old::String="MostLikelyStateController", new::String="MLRA") = return s == old ? new : s
 
-function create_eval_plot(data=nothing; filename::String="$(@__DIR__)/../results/final_results/data_eval_final.csv",
-                          outdir::String="$(@__DIR__)/../results/final_results/plots/",
-                          dims=(14.5cm, 10cm),
-                          reward_type::Symbol=:inf_discounted_reward)
-    if isnothing(data)
-        data = CSV.read(filename)
+function create_eval_plot(data=nothing;
+                          filename::String="$(@__DIR__)/../results/final_results/data_eval_final.csv",
+                          outdir::String="$(@__DIR__)/../results/final_results/plots/")
+
+    for reward_type in [:inf_discounted_reward, :undiscounted_reward]
+        if isnothing(data)
+            data = CSV.read(filename)
+        end
+
+        # value SEM
+        dims = (14.5cm, 10cm)
+        p = plot_results(data, show_nsteps=false, show_fst=false, show_value_violin=false, reward_type=reward_type)
+        draw(PDF(joinpath(outdir, "lp_value_sem_eval_plot-$(reward_type).pdf"), dims...), p)
+
+        # value violin
+        dims = (14.5cm, 10cm)
+        p = plot_results(data, show_nsteps=false, show_fst=false, show_value_sem=false, reward_type=reward_type)
+        draw(PDF(joinpath(outdir, "lp_value_violin_eval_plot-$(reward_type).pdf"), dims...), p)
+
+        # outcome histogram
+        dims = (14.5cm, 8cm)
+        p = plot_results(data, show_nsteps=false, show_fst=true, show_value_violin=false, show_value_sem=false, reward_type=reward_type)
+        draw(PDF(joinpath(outdir, "lp_outcome_eval_plot.pdf"), dims...), p)
+
+        # combined:
+        dims = (14.5cm, 21cm)
+        p = plot_results(data, show_nsteps=false, show_fst=false, reward_type=reward_type )
+        draw(PDF(joinpath(outdir, "lp_value_eval_plot-$(reward_type).pdf"), dims...), p)
     end
-
-    # value SEM
-    dims = (14.5cm, 10cm)
-    p = plot_results(data, show_nsteps=false, show_fst=false, show_value_violin=false, reward_type=reward_type)
-    draw(PDF(joinpath(outdir, "lp_value_sem_eval_plot-$(reward_type).pdf"), dims...), p)
-
-    # value violin
-    dims = (14.5cm, 10cm)
-    p = plot_results(data, show_nsteps=false, show_fst=false, show_value_sem=false, reward_type=reward_type)
-    draw(PDF(joinpath(outdir, "lp_value_violin_eval_plot-$(reward_type).pdf"), dims...), p)
-
-    # outcome histogram
-    dims = (14.5cm, 8cm)
-    p = plot_results(data, show_nsteps=false, show_fst=true, show_value_violin=false, show_value_sem=false, reward_type=reward_type)
-    draw(PDF(joinpath(outdir, "lp_outcome_eval_plot.pdf"), dims...), p)
-
-    # combined:
-    dims = (14.5cm, 21cm)
-    p = plot_results(data, show_nsteps=false, show_fst=false, reward_type=reward_type )
-    draw(PDF(joinpath(outdir, "lp_value_eval_plot-$(reward_type).pdf"), dims...), p)
 end
 
 # Renaming:
